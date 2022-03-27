f0e2758df3210f11e8cdeb5ec238a385eec3a518
Luis Leal <31942862+luisfelipe3d@users.noreply.github.com>
2022-03-27 03:14:35 -0300

    Initial commit

---
+++ src/test/java/br/unicap/meow/compiler/MainTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 luisfelipe3d
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.unicap.meow.compiler;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author internship
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+        String EXPECTED = "Hello, World!";
+        String GOT = "Hello, World!";
+        assertEquals(EXPECTED, GOT);
+    }
+    
+}
